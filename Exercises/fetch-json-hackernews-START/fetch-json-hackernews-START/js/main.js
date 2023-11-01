462ebf5c16a02a1a9d616c183ebe37c28cc504bc
FluffyWolf426 <122489861+FluffyWolf426@users.noreply.github.com>
2023-11-01 17:35:20 -0600

    ok

---
+++ Exercises/fetch-json-hackernews-START/fetch-json-hackernews-START/js/main.js
@@ -0,0 +1,27 @@
+/*
+Fetch Fundamentals
+In this example we're going to get the top news stories from 
+1. Select all stories and get stories button.
+    - Add an event listener on the button that will load the stories
+    (function for this cretaed below.)
+2. Create a function that will load all of the story ids.
+    - you'll use the fetch api on the top stories endpoint to do this.
+    - Documentation for top stories api endpoint https://github.com/HackerNews/API#new-top-and-best-stories
+3. Create a function that will fetch the story data using the id and
+    display is on the page.
+    - you'll use the fetch api on the items end point to do this.
+    - Documentation for items api endpoint https://github.com/HackerNews/API#items
+    - The HTML 
+    <a href="URL HERE" class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">TITLE HERE</h5>
+        <small>STORY SCORE HERE</small>
+        </div>
+        <p class="mb-1">TIMESTAMP HERE </p>
+        <small>AUTHOR HERE</small>
+    </a>
+4. Create a function that will convert the date into something readable
+    rather than a timestamp.
+
+*/
+
